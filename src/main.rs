c20b35a15e7e96793bc96481de5a1a42c9162b07
aahuinink <a_huinink@hotmail.ca>
2024-08-26 14:32:10 -0700

    fucking around

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+// pick a panicking behavior
+// use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+//  use panic_abort as _; // requires nightly
+// use panic_semihosting as _; // logs messages over ITM; requires ITM support
+use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::debug;
+
+#[entry]
+fn main() -> ! {
+    let roses = "blue";
+    
+    assert_eq!(roses, "red");
+
+    loop {
+        // your code goes here
+    }
+}
